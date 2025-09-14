0cb8d53fb3ff8bd875f5c353059b88b9c0e68a03
LucaKro <luc_kro@uni-bremen.de>
2025-09-14 21:48:24 +0200

    removed unused code

--- utils.py
+++ utils.py
@@ -67,8 +67,6 @@ def launch_rviz(config='pycram.rviz'):
 
 # Init visualization tools when running notebook on binderhub
 def _init_visual_tools(result):
-    # if result.error_in_exec is not None:
-    #     return
     display_desktop()
     sleep(3)
     launch_rviz()
